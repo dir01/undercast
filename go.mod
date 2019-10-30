7bc9e6046f5d4795cc11523f9308918096fd6c53
Andrew Zhu <dir01@dir01.org>
2019-10-30 10:13:49 +0200

    Repository got db-related structs and named queries

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/anacrolix/torrent v1.8.2
 	github.com/gorilla/mux v1.7.3
 	github.com/gorilla/websocket v1.4.1
+	github.com/jmoiron/sqlx v1.2.0
 	github.com/lib/pq v1.2.0
 )
--- server/repository.go
+++ server/repository.go
@@ -2,25 +2,34 @@ package server
 
 import (
 	"database/sql"
+	"encoding/json"
 	"errors"
 	"log"
+
+	"github.com/jmoiron/sqlx"
 )
 
 type repository struct {
-	db *sql.DB
+	db *sqlx.DB
 }
 
 func newRepository(db *sql.DB) *repository {
-	r := repository{db: db}
+	r := repository{db: sqlx.NewDb(db, "postgres")}
 	r.createTables()
 	return &r
 }
 
 func (r *repository) createTorrent(t *torrent) error {
-	err := r.db.QueryRow(
-		"INSERT INTO torrents(source) VALUES($1) RETURNING id",
-		t.Source).Scan(&t.ID)
+	dt := dbTorrentFromTorrent(t)
+	stmt, err := r.db.PrepareNamed(`INSERT INTO torrents(
+			state, name, source, filenames, bytes_completed, bytes_missing
+		) VALUES (
+			:state, :name, :source, :filenames, :bytes_completed, :bytes_missing
+		) RETURNING id`)
 	if err != nil {
+		return err
+	}
+	if err = stmt.Get(&t.ID, dt); err != nil {
 		return err
 	}
 	return nil
@@ -42,9 +51,9 @@ func (r *repository) getTorrentsList(start, count int) ([]torrent, error) {
 		var f string
 		if err := rows.Scan(&t.ID, &t.State, &t.Source, &f, &t.BytesCompleted, &t.BytesMissing); err != nil {
 			return nil, err
-		} else {
-			t.FileNames = append(t.FileNames, f)
 		}
+
+		t.FileNames = append(t.FileNames, f)
 		torrents = append(torrents, t)
 	}
 	return torrents, nil
@@ -75,9 +84,58 @@ CREATE TABLE IF NOT EXISTS torrents (
     CONSTRAINT require_source CHECK (
 		(case when source is null or length(source) = 0 then FALSE else TRUE end)
     )
-
 )`
 	if _, err := r.db.Exec(tableCreationQuery); err != nil {
 		log.Fatal(err)
 	}
+}
+
+type dbTorrent struct {
+	ID             int    `db:"id"`
+	State          string `db:"state"`
+	Name           string `db:"name"`
+	Source         string `db:"source"`
+	FileNames      string `db:"filenames"`
+	BytesCompleted int64  `db:"bytes_completed"`
+	BytesMissing   int64  `db:"bytes_missing"`
+}
+
+func dbTorrentFromTorrent(t *torrent) *dbTorrent {
+	return &dbTorrent{
+		ID:             t.ID,
+		State:          string(t.State),
+		Name:           t.Name,
+		Source:         t.Source,
+		FileNames:      marshalFilenames(t.FileNames),
+		BytesCompleted: t.BytesCompleted,
+		BytesMissing:   t.BytesMissing,
+	}
+}
+
+func (dt *dbTorrent) toEntity() *torrent {
+	return &torrent{
+		ID:             dt.ID,
+		State:          state(dt.State),
+		Name:           dt.Name,
+		Source:         dt.Source,
+		FileNames:      unmarshalFilenames(dt.FileNames),
+		BytesCompleted: dt.BytesCompleted,
+		BytesMissing:   dt.BytesMissing,
+	}
+}
+
+func marshalFilenames(filenames []string) string {
+	if f, err := json.Marshal(filenames); err == nil {
+		return string(f)
+	} else {
+		panic(err)
+	}
+}
+
+func unmarshalFilenames(fnStr string) (filenames []string) {
+	err := json.Unmarshal([]byte(fnStr), &filenames)
+	if err != nil {
+		panic(err)
+	}
+	return
 }
