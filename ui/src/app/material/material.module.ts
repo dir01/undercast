3316ce6cd6d9d5a89a15e838e7db6b901d07d951
Andrew Zhu <dir01@dir01.org>
2020-06-18 17:23:03 +0300

    ui: upgraded dependencies

--- ui/src/app/material/material.module.ts
+++ ui/src/app/material/material.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
-import {
-  MatInputModule,
-  MatProgressBarModule,
-  MatCardModule,
-  MatToolbarModule,
-  MatDividerModule,
-  MatIconModule,
-  MatExpansionModule,
-} from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 
 const MaterialComponents = [
